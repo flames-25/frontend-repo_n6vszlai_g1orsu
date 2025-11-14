6f37606af371715654d0d0489235985c51d0b4d3
Flames <vibe-coder@flames.com>
2025-11-14 16:08:14 +0000

    Update project prj_n6vszlai - 2025-11-14T16:08:13.609Z

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        {/* Catch all app routes so nested routes inside App (/, /courses, /contact, etc.) work */}
+        <Route path="/*" element={<App />} />
         <Route path="/test" element={<Test />} />
       </Routes>
     </BrowserRouter>
